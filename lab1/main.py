4c3944a709ba959f7acddf2551d043283935e4c6
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2020-03-25 11:50:49 +0100

    Added loop to automatically go through all graphs

--- lab1/main.py
+++ lab1/main.py
@@ -1,15 +1,56 @@
-from solutions.better_recursion import better_recursion
-from solutions.brute_force import brute_force
-from solutions.simple_recursion import simple_recursion
+from os import getcwd
+from os.path import join
+from vertex_cover.better_recursion import better_recursion
+from vertex_cover.kernelization import kernelize
+from vertex_cover.optimized_recursion import optimized_recursion
+from vertex_cover.simple_recursion import simple_recursion
 from utils.dimacs import *
 
-G = loadGraph("graphs/e5")
-V = len(G)
-for v in range(V):
-    s = "{0}:".format(v)
-    for u in G[v]:
-        s += " {0}".format(u)
-    print(s)
+graph_names = [
+    "e5",
+    "e10",
+    "e20",
+    "e40",
+    "e150",
+    "s25",
+    "s50",
+    "s500",
+    "b20",
+    "b30",
+    "b100",
+    "k330_a",
+    "k330_b",
+    "k330_c",
+    "m20",
+    "m30",
+    "m40",
+    "m50",
+    "m100",
+    "p20",
+    "p35",
+    "p60",
+    "p150",
+    "r30_01",
+    "r30_05",
+    "r50_001",
+    "r50_01",
+    "r50_05",
+    "r100_005"]
 
-sol = better_recursion(G, 4)
-print(isVC(edgeList(G), sol))
+graph_dir = join(getcwd(), "graphs")
+for name in graph_names:
+    graph_file = join(graph_dir, name)
+    solution_file = join(graph_dir, name + ".sol")
+    G = loadGraph(graph_file)
+    G_edge_list = edgeList(G)
+    for k in range(1, len(G)):
+        solution = optimized_recursion(G, k)
+        if not solution:
+            continue
+
+        print("sol", solution)
+        print(name, "k:", k)
+        print("VC:", isVC(G_edge_list, solution))
+        print()
+        saveSolution(solution_file, solution)
+        break
